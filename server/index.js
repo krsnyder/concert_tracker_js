bfc00669ecabeb3f3d538ce46fe91744b83159b8
Kirk Snyder <kirksnyder@Kirks-Laptop.local>
2024-09-24 13:00:29 -0400

    wip

---
+++ server/index.js
@@ -0,0 +1,9 @@
+import * as fs from "fs";
+
+const words = ["Hey", "ther", "dafd", "vdfsrvrs"];
+let output = words.join("\n");
+fs.writeFile("words.txt", output, writeComplete);
+
+function writeComplete() {
+  console.log("Write Complete!");
+}
